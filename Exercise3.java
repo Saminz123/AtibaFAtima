6450ab07787c2c2fa1919f499f1a915e2d25dcf4
Saminz123 <124643797+Saminz123@users.noreply.github.com>
2023-02-16 20:02:27 -0600

    Assignments Day 1

---
+++ Exercise3.java
@@ -0,0 +1,26 @@
+class Exercise3
+{
+public static void main (String args[])
+{
+//Single line comment
+System.out.println("Single line comment is displayed above");
+
+
+/* This is a multi line
+comment in a 
+Java program*/
+
+System.out.println("Multiple line comment is displayed above");
+
+
+/*
+//This is a nested comment
+ */
+
+System.out.println("Nested comment is displayed above");
+
+
+}
+
+
+}
---
+++ Exercise4.java
@@ -0,0 +1,16 @@
+class Exercise4 
+{
+  public static void main(String[] args) {
+    
+    int myInt=10;	
+    double myDouble = 3.4;
+    float myFloat = 3.4F;
+    long myLong=9849098490L;
+ 
+    System.out.println(myInt);     //prints 10
+    System.out.println(myDouble);  // prints 3.4
+    System.out.println(myFloat);    // prints 3.4
+    System.out.println(myLong);     //prints 9849098490
+    
+  }
+}
